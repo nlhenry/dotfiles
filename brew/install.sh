9db0e6e922ac73f99d9ede5c5bb6315546f9464a
nik <103350912+nlhenry@users.noreply.github.com>
2024-05-02 07:22:56 -0400

    Update install.sh

--- brew/install.sh
+++ brew/install.sh
@@ -1,3 +1,6 @@
+#This line is known as the shebang. This will tell the interpreter to run the Zsh program located in /bin/sh.
+#!/bin/zsh
+
 #Open up a terminal window and install homebrew with the following command:
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
