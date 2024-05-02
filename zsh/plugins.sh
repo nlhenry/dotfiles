2ffff4682467ed0e3e8522167e1efdff9904e834
nik <103350912+nlhenry@users.noreply.github.com>
2024-05-02 08:01:42 -0400

    Create plugins.sh

---
+++ zsh/plugins.sh
@@ -0,0 +1,13 @@
+
+
+#Install zsh-autosuggestions:
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+#Install zsh-syntax-highlighting:
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+#Open the ”~/.zshrc” file in your desired editor and modify the plugins line to what you see below.
+#plugins=(git zsh-autosuggestions zsh-syntax-highlighting web-search)
+
+#Load these new plugins by running:
+source ~/.zshrc
