2dbf3b1a3dc13f9a3e8e49bb3bd65d5c093e4150
nik <103350912+nlhenry@users.noreply.github.com>
2024-05-02 07:10:40 -0400

    Update install.sh

--- brew/install.sh
+++ brew/install.sh
@@ -1,1 +1,6 @@
+#Open up a terminal window and install homebrew with the following command:
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+#After installing, add it to the path (replace ”[username]” with your actual username):
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/nikolus.henry/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
