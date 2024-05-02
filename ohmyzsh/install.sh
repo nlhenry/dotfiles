77b9667a060c865fd2c53c0347565af6e5ed93d4
nik <103350912+nlhenry@users.noreply.github.com>
2024-05-02 08:50:51 -0400

    Update install.sh

--- ohmyzsh/install.sh
+++ ohmyzsh/install.sh
@@ -3,3 +3,9 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 #Run this to install PowerLevel10K:
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+#Now that it’s installed, open the ”~/.zshrc” file with your preferred editor and change the value of “ZSH_THEME” as shown below:
+#ZSH_THEME="powerlevel10k/powerlevel10k"
+
+#To reflect this change on your terminal, restart it or run this command:
+#source ~/.zshrc
