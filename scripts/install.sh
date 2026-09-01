035598a56302623b847bd88094620984e8ad3065
Eryn <103350912+luis-khon@users.noreply.github.com>
2026-09-01 19:00:08 -0400

    Create install.sh for GNOME setup
    Added a script to install GNOME configuration files.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+set -e
 
+cd ..
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+SRC_DIR="$SCRIPT_DIR/gnome"
+DEST_DIR="$HOME/.config/gnome"
+
+cp -a "$SRC_DIR" "$DEST_DIR"
+sudo bash "$DEST_DIR/config.sh"
